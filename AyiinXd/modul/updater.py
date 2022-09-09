e4abbc1eae825bac1798feeaf5daea4bdb9b4d75
Sip - userbot <110762610+sip-userbot@users.noreply.github.com>
2022-09-09 20:20:36 +0700

    Update and rename AyiinXd/modules/updater.py to AyiinXd/modul/updater.py

--- AyiinXd/modul/updater.py
+++ AyiinXd/modul/updater.py
@@ -130,7 +130,7 @@ async def upstream(event):
         xx = await eor(event, get_string("upd_1"))
     conf = event.pattern_match.group(1).strip()
     off_repo = b64decode(
-        "aHR0cHM6Ly9naXRodWIuY29tL2lhbXVwdXQvVXB1dHQtVXNlcmJvdCA"
+        "aHR0cHM6Ly9naXRodWIuY29tL2lhbXVwdXQvVXB1dHQtVXNlcmJvdCA="
     ).decode("utf-8")
     force_update = False
     try:
