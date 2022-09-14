ea8c21d48be74b4cae069617ac9a90edd886bd90
iamuput <111313150+iamuput@users.noreply.github.com>
2022-09-15 01:23:29 +0700

    Update utils.py

--- AyiinXd/ayiin/utils.py
+++ AyiinXd/ayiin/utils.py
@@ -77,7 +77,7 @@ async def autopilot():
     channel = get_peer_id(chat)
     if isinstance(chat.photo, ChatPhotoEmpty):
         photo = await download_file(
-            "https://telegra.ph/file/d88707f29d1bb4f9a42c7.jpg", "photoyins.jpg"
+            "https://telegra.ph/file/17770ad693a43ebeb0fb6.jpg", "photoyins.jpg"
         )
         ll = await bot.upload_file(photo)
         try:
@@ -135,7 +135,7 @@ async def autobot():
             sys.exit(1)
     filogo = random.choice(
           [
-              "https://telegra.ph/file/d88707f29d1bb4f9a42c7.jpg",
+              "https://telegra.ph/file/17770ad693a43ebeb0fb6.jpg",
               "AyiinXd/resources/IMG-20220912-WA0047.jpg",
           ]
     )
