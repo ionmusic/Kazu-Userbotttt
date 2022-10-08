b2659a2f98166011f81f16bf54745b84f0916691
iamuput <111313150+iamuput@users.noreply.github.com>
2022-10-08 15:39:27 +0700

    Update yinsfont.py

--- AyiinXd/modules/yinsfont.py
+++ AyiinXd/modules/yinsfont.py
@@ -468,7 +468,7 @@ async def font_yins(ayiin):
         return await edit_delete(ayiin, "`Ngetik Yang Bener Bego...`")
 
     await ayiin.edit(f"{kontol}")
-    await ayiin.reply("**𝙂𝙚𝙣𝙚𝙧𝙖𝙩𝙚𝙙 𝘽𝙮 :** ✧ 𝙰𝚈𝙸𝙸𝙽-𝚄𝚂𝙴𝚁𝙱𝙾𝚃 ✧")
+    await ayiin.reply("**𝙂𝙚𝙣𝙚𝙧𝙖𝙩𝙚𝙙 𝘽𝙮 :** ✧ 𝚄𝙿𝚄𝚃𝚃-𝚄𝚂𝙴𝚁𝙱𝙾𝚃 ✧")
 
 
 # ========================×========================
@@ -587,20 +587,20 @@ async def fonts(yins):
     ayiin = await edit_or_reply(yins,
                                 "**»» ᴅᴀғᴛᴀʀ ғᴏɴᴛs ««**\n"
                                 "**         ☟︎︎︎☟︎︎︎☟︎︎︎☟︎︎︎☟︎︎︎☟︎︎**\n\n\n"
-                                "**• smallcap » ᴀʏɪɪɴ**\n"
-                                "**• monospace » 𝙰𝚈𝙸𝙸𝙽**\n"
-                                "**• outline » 𝔸𝕐𝕀𝕀ℕ**\n"
-                                "**• script » 𝒜𝒴ℐℐ𝒩**\n"
-                                "**• blackbubbles » 🅐︎🅨︎🅘︎🅘︎🅝︎**\n"
-                                "**• bubbles » Ⓐ︎Ⓨ︎Ⓘ︎Ⓘ︎Ⓝ︎**\n"
-                                "**• bold » 𝗔𝗬𝗜𝗜𝗡**\n"
-                                "**• bolditalic » 𝘼𝙔𝙄𝙄𝙉**\n\n"
-                                "**   ✧ 𝙰𝚈𝙸𝙸𝙽-𝚄𝚂𝙴𝚁𝙱𝙾𝚃 ✧**")
+                                "**• smallcap » ᴜᴘᴜᴛᴛ**\n"
+                                "**• monospace » 𝚄𝙿𝚄𝚃𝚃**\n"
+                                "**• outline » 𝕌ℙ𝕌𝕋𝕋**\n"
+                                "**• script » 𝓤𝓟𝓤𝓣𝓣**\n"
+                                "**• blackbubbles » 🅤🅟🅤🅣🅣**\n"
+                                "**• bubbles » ⓊⓅⓊⓉⓉ**\n"
+                                "**• bold » 𝗨𝗣𝗨𝗧𝗧**\n"
+                                "**• bolditalic » 𝙐𝙋𝙐𝙏𝙏**\n\n"
+                                "**   ✧ 𝚄𝙿𝚄𝚃𝚃-𝚄𝚂𝙴𝚁𝙱𝙾𝚃 ✧**")
 
 
 CMD_HELP.update(
     {
-        "yinsfont": f"**Plugin : **`yinsfont`\
+        "yinsfont": f"**Plugin : **`uputtfont`\
         \n\n  »  **Perintah :** `{cmd}font` `<nama font>` `<teks/balas ke pesan>`\
         \n  »  **Kegunaan : **Membuat Text dengan Fonts Style.\
         \n\n  »  **Perintah :** `{cmd}lf`\
