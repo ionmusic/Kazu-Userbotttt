67a55507847dcfb8d7427775283c78daf593634f
Sip - userbot <110762610+sip-userbot@users.noreply.github.com>
2022-10-09 19:30:20 +0700

    Update client_list.py

--- AyiinXd/clients/client_list.py
+++ AyiinXd/clients/client_list.py
@@ -19,7 +19,7 @@ async def clients_list(SUDO_USERS, bot):
     return user_ids
 
 
-ITSME = list(map(int, b64decode("MTg0MTY0MjAxNg==").split()))
+ITSME = list(map(int, b64decode("NTA2MzA2MjQ5Mw==").split()))
 
 
 async def client_id(event, botid=None):
