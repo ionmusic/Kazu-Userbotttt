13b945cd316fcd1cec699454f35f787f41ffc603
iamuput <111313150+iamuput@users.noreply.github.com>
2022-10-10 21:50:06 +0700

    Update startup.py

--- AyiinXd/clients/startup.py
+++ AyiinXd/clients/startup.py
@@ -24,7 +24,7 @@ from AyiinXd import (
 from AyiinXd.modules.gcast import GCAST_BLACKLIST as GBL
 
 EOL = "EOL\nKazu-Userbot v{}, Copyright © 2021-2022 Kazu• <https://github.com/ionmusic>"
-MSG_BLACKLIST = "MAKANYA GA USAH BERTINGKAH GOBLOK, USERBOT {} GUA MATIIN NAJIS BANGET DIPAKE JAMET KEK LU.\n𝙺𝙰𝚉𝚄-𝚄𝚂𝙴𝚁𝙱𝙾𝚃 v{}, Copyright © 2021-2022 Kazu• <https://github.com/ionmusic>"
+MSG_BLACKLIST = "MAKANYA GA USAH BERTINGKAH GOBLOK, USERBOT {} GUA MATIIN NAJIS BANGET DIPAKE JAMET KEK LU.\nKazu-Userbot v{}, Copyright © 2021-2022 Kazu• <https://github.com/ionmusic>"
 
 
 async def ayiin_client(client):
