fb98200f0cfb5cb9677834b9c3d98811a0ea6879
izzy <50057075+hitokizzy@users.noreply.github.com>
2022-10-11 18:23:20 +0700

    Update startup.py

--- AyiinXd/clients/startup.py
+++ AyiinXd/clients/startup.py
@@ -71,6 +71,6 @@ def multiayiin():
         except Exception as e:
             LOGS.info(str(e))
 
-    if not STRING_SESSION:
-        failed += 1
-    return failed
+    #if not STRING_SESSION:
+    #    failed += 1
+    #return failed
