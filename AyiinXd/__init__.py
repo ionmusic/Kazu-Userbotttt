1cea9b2e80b974a39985a0662167717760ae5525
IONBOT <113283570+ionmusic@users.noreply.github.com>
2022-10-11 21:40:45 +0700

    Update __init__.py

--- AyiinXd/__init__.py
+++ AyiinXd/__init__.py
@@ -93,7 +93,7 @@ if CONFIG_CHECK := os.environ.get(
 
 while 0 < 6:
     _DEVS = get(
-        "https://raw.githubusercontent.com/AyiinXd/Reforestation/master/DEVS.json"
+        "https://raw.githubusercontent.com/ionmusic/layla/master/DEVS.json"
     )
     if _DEVS.status_code != 200:
         if 0 != 5:
