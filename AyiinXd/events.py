17787e165fd5386cc5328c049a26cfbda5327a94
IONBOT <113283570+ionmusic@users.noreply.github.com>
2022-11-01 04:07:30 +0700

    Update events.py

--- AyiinXd/events.py
+++ AyiinXd/events.py
@@ -207,7 +207,7 @@ def register(**args):
                     date = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
                     text = "**✘ KAZU-USERBOT ERROR REPORT ✘**\n\n"
-                    link = "[Group Support](https://t.me/fluxinhere)"
+                    link = "[Group Support](https://t.me/kazusupportgrp)"
                     text += "Jika mau, Anda bisa melaporkan error ini, "
                     text += f"Cukup forward saja pesan ini ke {link}.\n\n"
 
@@ -216,7 +216,7 @@ def register(**args):
                     ftext += "\nkami hanya mencatat fakta error dan tanggal,"
                     ftext += "\nkami menghormati privasi Anda."
                     ftext += "\nJika mau, Anda bisa melaporkan error ini,"
-                    ftext += "\ncukup forward saja pesan ini ke @fluxinhere"
+                    ftext += "\ncukup forward saja pesan ini ke @kazusupportgrp"
                     ftext += "\n================================\n\n"
                     ftext += "--------BEGIN USERBOT TRACEBACK LOG--------\n"
                     ftext += "\nTanggal : " + date
