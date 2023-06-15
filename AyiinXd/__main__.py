4c95d2f380af48b63fe08db9395607e59ff7d75f
naya1503 <118755946+naya1503@users.noreply.github.com>
2023-06-15 14:10:12 +0700

    update __main__.py

--- AyiinXd/__main__.py
+++ AyiinXd/__main__.py
@@ -19,13 +19,14 @@ from pytgcalls import __version__ as pytgcalls
 from telethon import version
 from telethon.tl.alltlobjects import LAYER
 
-from AyiinXd import BOT_TOKEN
+from AyiinXd import BOT_TOKEN, bot
 from AyiinXd import BOT_VER as ubotversion
 from AyiinXd import BOTLOG_CHATID, LOGS, LOOP, bot
 from AyiinXd.clients import ayiin_userbot_on, multiayiin
 from AyiinXd.core.git import git
 from AyiinXd.modules import ALL_MODULES
 from AyiinXd.ayiin import AyiinDB, HOSTED_ON, autobot, autopilot, ayiin_version
+from AyiinXd.ayiin import checking
 
 try:
     for module_name in ALL_MODULES:
@@ -47,6 +48,7 @@ except BaseException as e:
 
 
 LOOP.run_until_complete(ayiin_userbot_on())
+LOOP.run_until_complete(checking(bot))
 if not BOTLOG_CHATID:
     LOOP.run_until_complete(autopilot())
 if not BOT_TOKEN:
